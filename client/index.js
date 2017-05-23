73bb39c6667ec77e08322d2ff8bd5e7a20a03b79
pennsy <geniuscorona@gmail.com>
2017-05-23 16:38:39 +0800

    fix: index store

--- client/index.js
+++ client/index.js
@@ -3,14 +3,32 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
-import reducer from './reducers'
 import 'todomvc-app-css/index.css'
+import { configureStore } from './store';
+import { AppContainer } from 'react-hot-loader';
 
-const store = createStore(reducer)
+// Initialize store
+const store = configureStore(window.__INITIAL_STATE__);
+const mountApp = document.getElementById('root');
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+  <AppContainer>
+    <App store={store} />
+  </AppContainer>,
+  mountApp
+);
+
+// For hot reloading of react components
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    // If you use Webpack 2 in ES modules mode, you can
+    // use <App /> here rather than require() a <NextApp />.
+    const NextApp = require('./containers/App').default; // eslint-disable-line global-require
+    render(
+      <AppContainer>
+        <NextApp store={store} />
+      </AppContainer>,
+      mountApp
+    );
+  });
+}
